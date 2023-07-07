92e58c95398034ed69c67d82a44a39a60b229b90
Gardimy Charles <gardimycharles889@gmail.com>
2023-07-07 09:56:29 -0400

    Update Homepage.js and Detailspage.js file

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import axios from 'axios';
 import '../styles/Detailspage.css';
 
 function DetailsPage() {
@@ -9,82 +8,87 @@ function DetailsPage() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    axios
-      .get('https://api.jikan.moe/v4/anime?q=&sfw')
-      .then((res) => {
-        const { data } = res.data;
-        dispatch({ type: 'SET_CATEGORIES', payload: data });
+    fetch('https://api.jikan.moe/v4/anime?q=&sfw')
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: 'SET_CATEGORIES', payload: data.data });
       });
   }, [dispatch]);
 
-  const selectedCategory = categories.find((category) => category.mal_id === selectedCategoryId);
+  const selectedCategory = categories.find(
+    (category) => category.mal_id === selectedCategoryId,
+  );
 
   return (
     <div className="details_container">
       {selectedCategory && (
-      <>
-        <img className="images" src={selectedCategory.images.jpg.image_url} alt={selectedCategory.image_url} />
-        <table className="details_table" key={selectedCategory.mal_id}>
-          <tbody>
-            <tr>
-              <td className="bold">mal_id</td>
-              <td>{selectedCategory.mal_id}</td>
-            </tr>
-            <tr>
-              <td className="bold">title</td>
-              <td>{selectedCategory.title}</td>
-            </tr>
-            <tr>
-              <td className="bold">season</td>
-              <td>{selectedCategory.season}</td>
-            </tr>
-            <tr>
-              <td className="bold">episodes</td>
-              <td>{selectedCategory.episodes}</td>
-            </tr>
-            <tr>
-              <td className="bold">favorites</td>
-              <td>{selectedCategory.favorites}</td>
-            </tr>
-            <tr>
-              <td className="bold">status</td>
-              <td>{selectedCategory.status}</td>
-            </tr>
-            <tr>
-              <td className="bold">popularity</td>
-              <td>{selectedCategory.popularity}</td>
-            </tr>
-            <tr>
-              <td className="bold">members</td>
-              <td>{selectedCategory.members}</td>
-            </tr>
-            <tr>
-              <td className="bold">duration</td>
-              <td>{selectedCategory.duration}</td>
-            </tr>
-            <tr>
-              <td className="bold">rank</td>
-              <td>{selectedCategory.rank}</td>
-            </tr>
-            <tr>
-              <td className="bold">rating</td>
-              <td>{selectedCategory.rating}</td>
-            </tr>
-            <tr>
-              <td className="bold">score</td>
-              <td>{selectedCategory.score}</td>
-            </tr>
-            <tr>
-              <td className="bold">scored_by</td>
-              <td>{selectedCategory.scored_by}</td>
-            </tr>
-            <tr>
-              <td className="bold">year</td>
-              <td>{selectedCategory.year}</td>
-            </tr>
-          </tbody>
-        </table>
-      </>
+        <>
+          <img
+            className="images"
+            src={selectedCategory.images.jpg.image_url}
+            alt={selectedCategory.image_url}
+          />
+          <table className="details_table" key={selectedCategory.mal_id}>
+            <tbody>
+              <tr>
+                <td className="bold">mal_id</td>
+                <td>{selectedCategory.mal_id}</td>
+              </tr>
+              <tr>
+                <td className="bold">title</td>
+                <td>{selectedCategory.title}</td>
+              </tr>
+              <tr>
+                <td className="bold">season</td>
+                <td>{selectedCategory.season}</td>
+              </tr>
+              <tr>
+                <td className="bold">episodes</td>
+                <td>{selectedCategory.episodes}</td>
+              </tr>
+              <tr>
+                <td className="bold">favorites</td>
+                <td>{selectedCategory.favorites}</td>
+              </tr>
+              <tr>
+                <td className="bold">status</td>
+                <td>{selectedCategory.status}</td>
+              </tr>
+              <tr>
+                <td className="bold">popularity</td>
+                <td>{selectedCategory.popularity}</td>
+              </tr>
+              <tr>
+                <td className="bold">members</td>
+                <td>{selectedCategory.members}</td>
+              </tr>
+              <tr>
+                <td className="bold">duration</td>
+                <td>{selectedCategory.duration}</td>
+              </tr>
+              <tr>
+                <td className="bold">rank</td>
+                <td>{selectedCategory.rank}</td>
+              </tr>
+              <tr>
+                <td className="bold">rating</td>
+                <td>{selectedCategory.rating}</td>
+              </tr>
+              <tr>
+                <td className="bold">score</td>
+                <td>{selectedCategory.score}</td>
+              </tr>
+              <tr>
+                <td className="bold">scored_by</td>
+                <td>{selectedCategory.scored_by}</td>
+              </tr>
+              <tr>
+                <td className="bold">year</td>
+                <td>{selectedCategory.year}</td>
+              </tr>
+            </tbody>
+          </table>
+        </>
       )}
     </div>
   );
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import '../styles/Homepage.css';
 
 const HomePage = () => {
@@ -9,21 +8,28 @@ const HomePage = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    axios
-      .get('https://api.jikan.moe/v4/anime?q=&sfw')
-      .then((res) => {
-        const { data } = res.data;
-        dispatch({ type: 'SET_CATEGORIES', payload: data });
+    fetch('https://api.jikan.moe/v4/anime?q=&sfw')
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: 'SET_CATEGORIES', payload: data.data });
       });
   }, [dispatch]);
 
   return (
     <div className="grid-container">
       {categories.map((category) => (
-        <Link to={`/details/${category.mal_id}`} key={category.mal_id} onClick={() => dispatch({ type: 'SET_SELECTED_CATEGORY', payload: category.mal_id })}>
+        <Link
+          to={`/details/${category.mal_id}`}
+          key={category.mal_id}
+          onClick={() => dispatch({ type: 'SET_SELECTED_CATEGORY', payload: category.mal_id })}
+        >
           <div className="category-item">
             <div className="category-image-container">
-              <img className="category-image" src={category.images.jpg.image_url} alt={category.image_url} />
+              <img
+                className="category-image"
+                src={category.images.jpg.image_url}
+                alt={category.image_url}
+              />
               <div className="category-title">{category.title}</div>
               <div className="category-arrow-container">
                 <div className="category-arrow">&#10148;</div>
